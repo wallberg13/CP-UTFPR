5bd497ded4f15ae554359234cf79c051773e0e6d
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2018-08-15 13:45:10 -0300

    Nao sei o que deu com a pasta de projetos
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Sistemas-Distribuidos/MPI/pi.c
+++ Sistemas-Distribuidos/MPI/pi.c
@@ -6,17 +6,16 @@ int main(argc,argv)
 int argc;
 char *argv[];
 {
-	long long n = 5000000000,i;
+	long long n = 10000000000,i;
 	int myid, numprocs;
-    double PI25DT = 3.141592653589793238462643;
-    double mypi, pi, h, sum, x;
+	double PI25DT = 3.141592653589793238462643;
+	double mypi, pi, h, sum, x;
 	double start, end;
-    MPI_Init(&argc,&argv);
-    MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+    	MPI_Comm_rank(MPI_COMM_WORLD,&myid);
 	start = MPI_Wtime();
 	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
 	h   = 1.0 / (double) n;
 	sum = 0.0;
 	for (i = myid + 1; i <= n; i += numprocs) {
@@ -29,8 +28,8 @@ char *argv[];
 	end = MPI_Wtime();
 
 	if (myid == 0)
-	    printf("pi is approximately %.16f, Error is %.16f\nTime: %f",
-	pi, fabs(pi - PI25DT), end - start);
+	    printf("pi is approximately %.16f, Error is %.16f\nTime: %f\t Proc: %d\n",
+	pi, fabs(pi - PI25DT), end - start, numprocs);
     MPI_Finalize();
     return 0;
 }
